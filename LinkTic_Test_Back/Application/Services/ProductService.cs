ae7b44e583ec7cece9896cba0e2078ffcc293b62
Anderson_Méndez <amendez@yeminus.com>
2024-10-02 00:25:47 -0500

    Se agrega la lógica para el funcionamiento basico del endpoint de productos

--- LinkTic_Test_Back/Application/Services/ProductService.cs
+++ LinkTic_Test_Back/Application/Services/ProductService.cs
@@ -1,6 +1,44 @@
-﻿namespace LinkTic_Test_Back.Application.Services
+﻿using LinkTic_Test_Back.Domain.Entities;
+using LinkTic_Test_Back.Domain.Interfaces;
+using LinkTic_Test_Back.Domain.Services;
+
+namespace LinkTic_Test_Back.Application.Services
 {
-    public class ProductService
+    public class ProductService : IProductService
     {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            return await _productRepository.GetAllAsync();
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _productRepository.GetByIdAsync(id);
+        }
+
+        public async Task CreateProductAsync(Product product)
+        {
+            if (string.IsNullOrEmpty(product.Name) || product.Price <= 0)
+                throw new ArgumentException("Invalid product data");
+
+            await _productRepository.AddAsync(product);
+        }
+
+        public async Task UpdateProductAsync(Product product)
+        {
+            await _productRepository.UpdateAsync(product);
+        }
+
+        public async Task DeleteProductAsync(int id)
+        {
+            await _productRepository.DeleteAsync(id);
+        }
     }
 }
--- LinkTic_Test_Back/Program.cs
+++ LinkTic_Test_Back/Program.cs
@@ -1,3 +1,9 @@
+using LinkTic_Test_Back.Application.Services;
+using LinkTic_Test_Back.Domain.Interfaces;
+using LinkTic_Test_Back.Domain.Services;
+using LinkTic_Test_Back.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// Agrega las dependencias
+builder.Services.AddScoped<IProductService, ProductService>();
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
+// Agrega el contexto de la base de datos
+builder.Services.AddDbContext<ECommerceContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Agrega los controladores
+builder.Services.AddControllers();
+
 
 var app = builder.Build();
 
